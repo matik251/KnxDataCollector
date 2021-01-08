930a777db77f2d96096826251208be88884db0c9
matik251 <mateusz.pawel.kula@gmail.com>
2021-01-08 17:44:00 +0100

    Dodanie Home API, MVC

---
+++ Model/Home.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KnxDataCollector.Model
+{
+    public partial class Home
+    {
+        public int ID { get; set; }
+        public string? HomeText { get; set; }
+        public DateTime? Time { get; set; }
+    }
+}
