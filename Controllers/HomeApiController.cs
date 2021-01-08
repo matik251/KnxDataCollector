26954febdd0010a3798a9bfa1d029897b273a4f7
matik251 <mateusz.pawel.kula@gmail.com>
2021-01-08 17:44:00 +0100

    Dodanie Home API, MVC

---
+++ Controllers/HomeApiController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KnxDataCollector.Model;
+
+namespace KnxDataCollector.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class HomeApiController : ControllerBase
+    {
+        private readonly KnxDBContext _context;
+
+        public HomeApiController(KnxDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/HomeApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Home>>> GetHome()
+        {
+            return await _context.Home.ToListAsync();
+        }
+
+        // GET: api/HomeApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Home>> GetHome(int id)
+        {
+            var home = await _context.Home.FindAsync(id);
+
+            if (home == null)
+            {
+                return NotFound();
+            }
+
+            return home;
+        }
+
+        // PUT: api/HomeApi/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHome(int id, Home home)
+        {
+            if (id != home.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(home).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HomeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/HomeApi
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Home>> PostHome(Home home)
+        {
+            _context.Home.Add(home);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetHome", new { id = home.ID }, home);
+        }
+
+        // DELETE: api/HomeApi/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Home>> DeleteHome(int id)
+        {
+            var home = await _context.Home.FindAsync(id);
+            if (home == null)
+            {
+                return NotFound();
+            }
+
+            _context.Home.Remove(home);
+            await _context.SaveChangesAsync();
+
+            return home;
+        }
+
+        private bool HomeExists(int id)
+        {
+            return _context.Home.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Controllers/HomesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KnxDataCollector.Model;
+using Microsoft.AspNetCore.Cors;
+
+namespace KnxDataCollector.Controllers
+{
+    [Route("api/mvc")]
+    [ApiController]
+    public class HomesController : Controller
+    {
+        private readonly KnxDBContext _context;
+
+        public HomesController(KnxDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Homes
+        [HttpGet]
+        [EnableCors("AllowOrigin")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Home.ToListAsync());
+        }
+
+        // GET: Homes/Details/5
+        [HttpGet("{id}")]
+        [EnableCors("AllowOrigin")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var home = await _context.Home
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (home == null)
+            {
+                return NotFound();
+            }
+
+            return View(home);
+        }
+
+        //// GET: Homes/Create
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        // POST: Homes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,HomeText,Time")] Home home)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(home);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(home);
+        }
+
+        private bool HomeExists(int id)
+        {
+            return _context.Home_1.Any(e => e.ID == id);
+        }
+    }
+}
